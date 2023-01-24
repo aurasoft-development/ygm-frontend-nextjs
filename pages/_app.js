520315a3f0d420006b95519bd3b2789038a6e6c6
harshitadubey <harshitadubeybpl@gmail.com>
2023-01-24 11:29:53 +0530

    ygm slider and header added slider not workking yet

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,22 @@ import '../styles/globals.css'
 import { ToastContainer } from 'react-toastify';
 import { useState } from 'react';
 import Router from 'next/router';
-import Loader from '../components/Loader';
+// import Loader from '../components/Loader';
 // This section is for new css import
 import '../styles/WhatsappButton.css';
 import '../styles/ScrollTopButton.css';
 import '../styles/EnquiryButton.css';
+import '../styles/slider.css';
+import '../styles/header.css';
+import '../styles/popupform.css';
+import '../styles/contact.module.css';
 import WhatsappButton from '../components/WhatsappButton';
 import ScrollTopButton from '../components/ScrollTopButton';
-import EnquiryButton from '../components/Enquiry';
+import Enquiry from "../components/Enquiry";
+import HomeSlider from '../components/HomeSlider';
+import Modal from 'react-bootstrap/Modal';
+import ContactForm from '../components/ContactForm';
+
 
 
 function MyApp({ Component, pageProps }) {
@@ -21,11 +29,22 @@ function MyApp({ Component, pageProps }) {
   Router.events.on('routeChangeComplete', (url) => {
     setLoading(false)
   })
+  const [show, setShow] = useState(false);
+  const handleClose = () => {
+    setShow(false);
+  }
+  const handleShow = () => {
+    setShow(true);
+  }
   return <Layout>
-    {Loading && <Loader />}
+    {/* {Loading && <Loader />} */}
+    <HomeSlider/>
+    <Enquiry setOpen={handleShow} />
     <ToastContainer position="top-right" limit={1} />
     <Component {...pageProps} />
-<EnquiryButton/>
+    <Modal show={show} onHide={handleClose}>
+          <ContactForm onCloseModel={handleClose} />
+        </Modal>
 <WhatsappButton/>
 <ScrollTopButton/>
   </Layout>
